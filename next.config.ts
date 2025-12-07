efb19ebcad05be6b98a7588494491fecb59572f4
Carlos Daniel Corrales <carlox59681@gmail.com>
2025-12-07 18:05:39 -0500

    next config added

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Esto silencia Turbopack y permite Webpack sin conflicto
+  turbopack: {
+    rules: {},
+  },
+
   redirects: async () => {
     return [
       {
@@ -10,15 +15,32 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_SUPABASE_URL?.replace('https://', '') || "wkthjrftwyiwenmwuifl.supabase.co",
+        hostname:
+          process.env.NEXT_PUBLIC_SUPABASE_URL?.replace("https://", "") ||
+          "wkthjrftwyiwenmwuifl.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/**",
       },
     ],
+  },
+
+  serverExternalPackages: ["pino", "pino-pretty", "thread-stream"],
+
+  webpack: (config) => {
+    config.externals.push({
+      tap: "commonjs tap",
+      "why-is-node-running": "commonjs why-is-node-running",
+      "pino-elasticsearch": "commonjs pino-elasticsearch",
+      fastbench: "commonjs fastbench",
+      desm: "commonjs desm",
+    });
+
+    return config;
   },
 };
 
